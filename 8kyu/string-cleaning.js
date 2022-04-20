3bf988873d892e52ee695744fa3e0c8db6a72655
Thiago Barros <88802518+thiagotesla@users.noreply.github.com>
2022-04-20 12:14:41 -0300

    create string-cleaning.js

---
+++ 8kyu/string-cleaning.js
@@ -0,0 +1,9 @@
+// Your boss decided to save money by purchasing some cut-rate optical character 
+// recognition software for scanning in the text of old novels to your database.
+// At first it seems to capture words okay, but you quickly notice that it throws
+// in a lot of numbers at random places in the text.
+
+
+function stringClean(string){
+    return string.replace(/[0-9]/g,'')
+}
